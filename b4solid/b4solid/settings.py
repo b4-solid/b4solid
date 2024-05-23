9b5ddf6a222cf691e581c07c685b8233a0eac25a
aldenluthfi <aldenluthfi@gmail.com>
2024-05-23 21:40:36 +0700

    Build Test

--- b4solid/b4solid/settings.py
+++ b4solid/b4solid/settings.py
@@ -143,9 +143,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/staticfiles/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
