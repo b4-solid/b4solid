5d2ffcdb68021f9886db7039c04553802633bb52
aldenluthfi <aldenluthfi@gmail.com>
2024-05-24 16:34:06 +0700

    Rebuild fix

--- b4solid/administration/views.py
+++ b4solid/administration/views.py
@@ -15,6 +15,6 @@ def products(request):
         product['id_label'] = f'#{product["id"]:08x}'
 
     if request.method == 'POST':
-        rq.delete('http://aldenluth.fi/products/' + request.POST['id'])
+        rq.delete('http://aldenluth.fi:8082/products/' + request.POST['id'])
 
     return render(request, 'products.html', context=context)
