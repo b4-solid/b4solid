162a0809cb26db232c56c22be878006078cfef49
aldenluthfi <aldenluthfi@gmail.com>
2024-05-24 23:27:38 +0700

    FInished Product CRUD Build

--- b4solid/administration/urls.py
+++ b4solid/administration/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from administration.views import requests, products
+from administration.views import requests, products, add_product, edit_product
 
 app_name = 'administration'
 
 urlpatterns = [
     path('requests/', requests, name='requests'),
     path('products/', products, name='products'),
+    path('add_product/', add_product, name='add_product'),
+    path('edit_product/<int:id>/', edit_product, name='edit_product')
 ]
--- b4solid/administration/views.py
+++ b4solid/administration/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import redirect, render
 import requests as rq
 
 def requests(request):
+    if not request.session.get('godmode'):
+        return redirect('main:main')
+
     return render(request, 'requests.html')
 
 def products(request):
+    if not request.session.get('godmode'):
+        return redirect('main:main')
 
     if request.method == 'POST':
         rq.delete('http://aldenluth.fi:8082/products/' + request.POST['id'])
@@ -15,6 +21,40 @@ def products(request):
     context['products'] = products_all
 
     for product in products_all:
-        product['id_label'] = f'#{product["id"]:08x}'
+        product['id_label'] = f'#{product["id"]:08X}'
 
     return render(request, 'products.html', context=context)
+
+def add_product(request):
+    if not request.session.get('godmode'):
+        return redirect('main:main')
+
+    if request.method == 'POST':
+        rq.post(
+            url='http://aldenluth.fi:8082/products',
+            data=json.dumps(request.POST),
+            headers={'Content-Type': 'application/json'}
+        )
+        return redirect('administration:products')
+
+    return render(request, 'add_product.html')
+
+def edit_product(request, id):
+    if not request.session.get('godmode'):
+        return redirect('main:main')
+
+    if request.method == 'POST':
+        rq.put(
+            url='http://aldenluth.fi:8082/products/' + str(id),
+            data=json.dumps(request.POST),
+            headers={'Content-Type': 'application/json'}
+        )
+        return redirect('administration:products')
+
+    product = rq.get('http://aldenluth.fi:8082/products/' + str(id)).json()
+    product['id_label'] = f'#{product["id"]:08X}'
+
+    context = {}
+    context['product'] = product
+
+    return render(request, 'edit_product.html', context=context)
